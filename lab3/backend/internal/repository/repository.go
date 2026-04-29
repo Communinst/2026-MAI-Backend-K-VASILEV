b2c2422cf4fba0aa516ae0b8db457bffb85ee43a
Communist <kirixa87@gmail.com>
2026-04-29 20:48:28 +0300

    lab6

--- lab3/backend/internal/repository/repository.go
+++ lab3/backend/internal/repository/repository.go
@@ -10,6 +10,8 @@ type ProductRepositoryInterface interface {
 	GetAll(ctx context.Context) ([]models.Product, error)
 	Create(ctx context.Context, product *models.Product) (uint, error)
 	GetByID(ctx context.Context, id uint64) (*models.Product, error)
+	Update(ctx context.Context, id uint64, product *models.Product) error
+	Delete(ctx context.Context, id uint64) error
 	Search(ctx context.Context, query string) ([]models.Product, error)
 }
 
